89b0f52c22855178eb958b73a0ae924b5745a7df
sariiiiiiii <abdf13423@naver.com>
2021-11-07 23:13:18 +0900

    11/07

--- src/main/java/com/iei/greenlight/auction/store/AuctionStore.java
+++ src/main/java/com/iei/greenlight/auction/store/AuctionStore.java
@@ -1,5 +1,20 @@
 package com.iei.greenlight.auction.store;
 
-public interface AuctionStore {
+import java.util.HashMap;
+import java.util.List;
 
+import com.iei.greenlight.auction.domain.Auction;
+import com.iei.greenlight.auction.domain.AuctionImage;
+
+public interface AuctionStore {
+	
+	// auction
+	public int insertAuction(Auction auction);
+	
+	public List<Auction> selectAllList();
+
+	
+	//auctinoImage
+	public int insertAuctionImage(List<AuctionImage> aList);
+	
 }
--- src/main/java/com/iei/greenlight/user/controller/UserController.java
+++ src/main/java/com/iei/greenlight/user/controller/UserController.java
@@ -38,8 +38,5 @@ public class UserController {
 		}else {
 			return"user/error";
 		}
-		
 	}
-	
-	
 }
